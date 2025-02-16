295e0860bf64e17ac17c3eeb14becf24f3f76f2a
v-Kaefer <128633407+v-Kaefer@users.noreply.github.com>
2025-02-16 03:22:51 -0300

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,22 +3,11 @@
 
 mod github_api;
 mod file_manager;
-mod integrations;
+mod integration_version;
 
-use github_api::{fetch_repo_contents, resolve_download_url};
-use file_manager::{is_process_running, download_file};
-use integrations::{verify_latest_version};
+use file_manager::download_file;
+use integration_version::verify_latest_version;
 
-use std::fs;
-use std::fs::File;
-use std::process::Command;
-use reqwest::blocking;
-use std::io::copy;
-use sysinfo::{ProcessExt, System, SystemExt};
-use std::path::Path;
-
-
-// Função principal
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
@@ -26,5 +15,5 @@ fn main() {
             verify_latest_version
         ])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Error while running Tauri application");
 }
